0a547789baf670c6ccaaa42d1bfe27bda744d42a
jason <jluce@DESKTOP-LOTUM80.localdomain>
2022-04-24 11:45:07 -0700

    tests

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,11 +2,11 @@ import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
 // comment
-@Controller()
+@Controller('ping')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get('ping')
+  @Get()
   getHello(): string {
     return this.appService.getHello();
   }
