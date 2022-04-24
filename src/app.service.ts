4763f776c33ce71936b19f020b8c62890fc9b7bf
jlucero805 <71980378+jlucero805@users.noreply.github.com>
2022-04-24 12:11:42 -0700

    test

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,9 +3,7 @@ import { Pool } from 'pg';
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
+  ssl: true,
 });
 
 @Injectable()
